f35266ec2f10e3590661827328373aebb030076c
Васильева <@223_15>
2018-03-28 20:27:10 +0600

    Class

---
+++ ConsoleApplication1/Modules/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Modules
+{
+    public class Product
+    {
+        public Product(): this(0) {}
+        public Product(int Pin):this(Pin,"") {}
+        public Product(int Pin, string Brend) {}
+
+
+
+        public int Pin { get; set; } // prop and double tab
+        public string Brend { get; set; }
+        public string Name { get; set; }
+        public bool Analog { get; set; }
+        public string ArtId { get; set; }
+
+        private double Price_;
+        public double Price
+        {
+            get { return Price_; }
+            set
+            {
+                if (value > 0)
+                {
+                    Price_ = value;
+                }
+                else
+                    Price_ = 0;
+
+                //(value > )? Price_ = value : Price_ = 0;
+            }
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApplication1.Modules;
 
 namespace ConsoleApplication1
 {
@@ -10,8 +11,18 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
-            Console.ReadKey();
+            List<Product> products = new List<Product>();
+            int productCount = 0;
+            Console.WriteLine("Pleas enter count of products");
+            Int32.TryParse(Console.ReadLine(), out productCount);
+            Random r = new Random();
+
+            for (int i = 0; i < productCount; i++)
+            {
+                Product product = new Product();
+                product.Pin = r.Next(0, 1458435);
+                product.Brend = Console.ReadLine();
+            }
         }
     }
 }
