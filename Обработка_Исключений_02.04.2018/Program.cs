af549284043d2259d2496c1f58ddb16d715a9d53
Васильева <@223_15>
2018-04-02 19:59:28 +0600

    try

---
+++ Обработка_Исключений_02.04.2018/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обработка_Исключений_02._04._2018 // Try Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region try
+            try
+            {
+            int[] mas = new int[10];
+                Console.WriteLine(mas[23]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
+
+            
+        }
+    }
+}
